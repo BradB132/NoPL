dcb549806ced732a85fad49bf97ab8b74caa4852
Brad Bambara <brad.bambara@smallplanet.com>
2012-10-18 10:53:05 -0400

    adding a file browser to NoPLGanger

---
+++ NoPLGanger/NoPLGanger/DataList/DataListController.m
@@ -0,0 +1,13 @@
+//
+//  DataListController.m
+//  NoPLGanger
+//
+//  Created by Brad Bambara on 10/17/12.
+//  Copyright (c) 2012 Brad Bambara. All rights reserved.
+//
+
+#import "DataListController.h"
+
+@implementation DataListController
+
+@end
