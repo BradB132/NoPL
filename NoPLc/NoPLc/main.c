18a1f9ca9252a9a8b6d78bc3e3937d7c1214be01
Brad Bambara <brad.bambara@gmail.com>
2013-04-21 11:53:47 -0400

    adding executable for NoPLc so that it can be used from command line, fixing up main function for said executable so that it's easier to use

--- NoPLc/NoPLc/main.c
+++ NoPLc/NoPLc/main.c
@@ -7,14 +7,16 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "NoPLc.h"
 
 int main(int argc, const char * argv[])
 {
 	//check to make sure we have 2 paths specified
-	if(argc < 3)
+	if(argc < 2)
 	{
-		printf("NoPLc requires 2 arguments: input source path and compiled output path");
+		printf("NoPLc usage: [input source path] [optional: compiled output path]");
 		return -1;
 	}
 	
@@ -23,7 +25,6 @@ int main(int argc, const char * argv[])
 	
 	//set up the compile options
 	NoPL_CompileOptions compileOpt = NoPL_CompileOptions();
-	compileOpt.createTokenRanges = 1;
 	
 	//compile the context
 	compileContextWithFilePath(argv[1], &compileOpt, &compileCtx);
@@ -52,10 +53,30 @@ int main(int argc, const char * argv[])
 			}
 		}
 		
+		//get a copy of the output path that we want
+		char* outputPath = NULL;
+		if(argc > 2)
+		{
+			outputPath = (char*)malloc(strlen(argv[2])+1);
+			strcpy(outputPath, argv[2]);
+		}
+		else
+		{
+			//this path should have an extension of 'nopl', we want to convert that to 'noplb'
+			long arg1Length = strlen(argv[1]);
+			outputPath = (char*)malloc(arg1Length+2);
+			strcpy((char*)outputPath, argv[1]);
+			outputPath[arg1Length] = 'b';
+			outputPath[arg1Length+1] = '\0';
+		}
+		
 		//no errors, save the output to file
-		FILE* file = fopen(argv[2], "wb");
+		FILE* file = fopen(outputPath, "wb");
 		fwrite(compileCtx.compiledData, 1, compileCtx.dataLength, file);
 		fclose(file);
+		
+		//clean up the string copy we did earlier
+		free(outputPath);
 	}
 	
 	//free the context
